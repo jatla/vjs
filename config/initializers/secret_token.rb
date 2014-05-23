04624ec1d24473e406c761f646f62cd134fad718
JayaPrakash Reddy Atla <jatla@Sunithas-MacBook-Air.local>
2014-05-23 10:34:59 -0700

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vjs::Application.config.secret_key_base = '131759d347eb72c7b907081991dfd3b830e35288835d9e080ec88bc440920ba05bd156a73a124045563691b7a6442fc8926609cf2bec1c0e3da8f5b829128b78'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vjs::Application.config.session_store :cookie_store, key: '_vjs_session'
